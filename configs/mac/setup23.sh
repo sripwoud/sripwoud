d0088ef10609bb93b673516a5a73fd7a55c1ab5c
3pwd <me@3pwd.xyz>
2023-04-08 23:55:53 +0200

    fix get extra urls

--- configs/mac/setup23.sh
+++ configs/mac/setup23.sh
@@ -1,3 +1,42 @@
+set -o errexit
+
+get_extra_urls() {
+	{
+		echo "https://updates.signal.org/desktop/$(curl -s https://updates.signal.org/desktop/latest-mac.yml | dasel -r yaml 'files.[2].url')";
+		curl -s https://api.github.com/repos/balena-io/etcher/releases/latest | dasel -r json 'assets.all().browser_download_url' | grep 'dmg"' | cut -d '"' -f 2;
+		curl -s https://api.github.com/repos/standardnotes/app/releases/latest | dasel -r json 'assets.all().browser_download_url' | grep 'x64.dmg"' | cut -d '"' -f 2;
+		curl -s -G "https://data.services.jetbrains.com/products/releases" -d "code=TBA" -d "latest=true" | dasel -r json "TBA.[0].downloads.mac.link" | tr -d '"'
+	} >>/tmp/installers/list
+}
+
+download_installers() {
+	while read -r url; do
+		wget --https-only --show-progress --content-disposition -nv -P /tmp/installers "$url"
+	done </tmp/installers/list
+}
+
+get_installers() {
+	mkdir -p /tmp/installers
+	curl -fSL https://raw.githubusercontent.com/3pwd/3pwd/mac/configs/mac/installers -o /tmp/installers/list
+	get_extra_urls
+	download_installers
+}
+
+function install_dmg {
+    volume=$(sudo hdiutil attach "$1" | grep Volumes | cut -f 3)
+
+    for app in "$volume"/*(.app|.pkg); do
+      case $app in
+        *.app) sudo cp -rf "$app" /Applications ;;
+        *.pkg)
+          sudo installer -pkg "$app" -target / ;;
+      esac
+    done
+
+    sudo hdiutil detach "$volume"
+    rm -rf "$1"
+}
+
 main() {
 	# Fira Code Font
 	curl -fsSL https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip -o /tmp/Fira_Code_v6.2.zip
@@ -22,7 +61,18 @@ main() {
 		asdf global "$plugin" latest
 	done <asdf-plugins
 
-	sh get_installers.sh
+	get_installers
+
+	for app in /tmp/installers/*(.tar.gz|.zip|.dmg); do
+    case $app in
+      *.dmg)
+        install_dmg "$app" ;;
+      *.tar.gz)
+        tar -xzf "$app" -C /tmp >/dev/null ;;
+      *.zip)
+        unzip "$app" -d /tmp >/dev/null ;;
+    esac
+  done
 }
 
 main "$@"
