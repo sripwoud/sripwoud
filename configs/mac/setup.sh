e7ffa5bd51fa9039c8fe37259a1f01cee8bbafb2
3pwd <me@3pwd.xyz>
2023-04-09 15:46:18 +0200

    refactor ubuntu script
    Un tested

--- configs/mac/setup.sh
+++ configs/mac/setup.sh
@@ -2,7 +2,7 @@ set -e
 
 main() {
   for i in {1..3}; do
-    curl -fsS https://raw.githubusercontent.com/3pwd/3pwd/master/configs/common/setup"$i"3.sh | sh
+    curl -fsS https://raw.githubusercontent.com/3pwd/3pwd/master/configs/mac/setup"$i"3.sh | sh
   done
 }
 
