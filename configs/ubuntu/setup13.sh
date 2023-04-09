e5f1ac6119d7a6b9043e045e1f89acca07ff691b
sripwoud <me@sripwoud.xyz>
2023-04-09 15:47:00 +0200

    refactor ubuntu script (#3)
    Un tested

--- configs/ubuntu/setup13.sh
+++ configs/ubuntu/setup13.sh
@@ -1,12 +1,9 @@
-# Git
-sudo apt install git -y
-curl https://github.com/web-flow.gpg | gpg --import
+set -e
 
-# Zsh
-sudo apt install zsh -y
-echo "Make zsh default shell"
-chsh -s "$(which zsh)"
+main() {
+  sudo apt install git -y
+  sudo apt install zsh -y
+  chsh -s "$(which zsh)"
+}
 
-# Oh My Zsh
-echo "Install oh my zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+main "$@"
