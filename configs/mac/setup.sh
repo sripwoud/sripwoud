eb49117fc4aea41ed893881d5f3bcf735668a877
sripwoud <me@sripwoud.xyz>
2023-06-23 19:53:29 +0200

    Update setup.sh

--- configs/mac/setup.sh
+++ configs/mac/setup.sh
@@ -2,6 +2,7 @@ set -e
 
 main() {
   for i in {1..3}; do
+    # TODO: fix, need to DL .zshrc file for mac before running e.g asdf install script so that asdf command is in PATH
     curl -fsS https://raw.githubusercontent.com/sripwoud/sripwoud/master/configs/mac/setup"$i"3.sh | sh
   done
 }
