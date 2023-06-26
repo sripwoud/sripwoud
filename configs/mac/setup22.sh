2ed4b3b5abfe24bc309db9a89e0df41a81888058
sripwoud <me@sripwoud.xyz>
2023-06-26 20:45:52 +0200

    Add standard notes installer url

--- configs/mac/setup22.sh
+++ configs/mac/setup22.sh
@@ -16,6 +16,7 @@ get_extra_urls() {
     curl -s https://api.github.com/repos/balena-io/etcher/releases/latest | dasel -r json 'assets.all().browser_download_url' | grep 'dmg"' | cut -d '"' -f 2
     curl -s https://api.github.com/repos/standardnotes/app/releases/latest | dasel -r json 'assets.all().browser_download_url' | grep 'x64.dmg"' | cut -d '"' -f 2
     curl -s -G "https://data.services.jetbrains.com/products/releases" -d "code=TBA" -d "latest=true" | dasel -r json "TBA.[0].downloads.mac.link" | tr -d '"'
+    curl -s 'https://standardnotes.com/page-data/index/page-data.json' | jq '.result.pageContext.downloads.downloads[].apps[] | select(.platform == "mac-intel") | .link' | tr -d '"'
   } >>/tmp/list
 }
 
