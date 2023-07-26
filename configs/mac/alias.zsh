c470a54ce0542473c4ca3205d4d0807831e00fdb
sripwoud <me@sripwoud.xyz>
2023-07-26 22:26:51 +0200

    Update alias.zsh

--- configs/mac/alias.zsh
+++ configs/mac/alias.zsh
@@ -1,5 +1,5 @@
 alias busyports='lsof -i'
-alias compresspdf=ps2pdf -dPDFSETTINGS=/screen
+alias compresspdf="ps2pdf -dPDFSETTINGS=/screen"
 alias fmtsh='shfmt -i 2 -w'
 alias grf='git update-ref -d HEAD'
 alias plm=pulumi
