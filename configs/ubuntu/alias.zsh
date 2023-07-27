fa5f0e8be8ea8477ced48e18fb879649dbd5ce17
sripwoud <me@sripwoud.xyz>
2023-07-27 14:58:46 +0200

    Update alias.zsh

--- configs/ubuntu/alias.zsh
+++ configs/ubuntu/alias.zsh
@@ -1,11 +1,12 @@
-alias busyports='lsof -i'
+alias busyports="lsof -i"
 alias -g clip="| xsel -ib"
-alias compresspdf="ps2pdf -dPDFSETTINGS=/screen"
-alias discord='flatpak run com.discordapp.Discord'
+alias compresspdf="ps2pdf -dPDFSETTINGS=/ebook"
+alias discord="flatpak run com.discordapp.Discord"
 alias fly=flyctl
-alias fmtsh='shfmt -i 2 -w'
+alias fmtsh="shfmt -i 2 -w"
 alias plm=pulumi
 alias pn=pnpm
-alias rgf='rg --files 2> /dev/null | rg'
+alias reloadzsh="source ~/.zshrc"
+alias rgf="rg --files 2> /dev/null | rg"
 alias update="aguu && sudo $APT dist-upgrade && agar && aga"
 alias zshconfig="open ~/.zshrc"
