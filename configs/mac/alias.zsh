71802b409f5edfb686db68f355dde0c862f4e6eb
sripwoud <me@sripwoud.xyz>
2023-08-06 21:14:58 +0200

    rebase

--- configs/mac/alias.zsh
+++ configs/mac/alias.zsh
@@ -1,7 +1,10 @@
 alias busyports='lsof -i'
 alias compresspdf="ps2pdf -dPDFSETTINGS=/screen"
+alias edit='open -a "TextEdit"'
+alias fly=flyctl
 alias fmtsh='shfmt -i 2 -w'
 alias grf='git update-ref -d HEAD'
 alias plm=pulumi
 alias pn=pnpm
+alias reloadzsh="source ~/.zshrc"
 alias zshconfig="open ~/.zshrc"
