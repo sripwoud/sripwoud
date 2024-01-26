87582171cbbd9e866ddc198fbffb1868cfba0880
sripwoud <me@sripwoud.xyz>
2024-01-26 09:21:11 +0100

    update ubuntu alias and default-npm-packages

--- configs/ubuntu/alias.zsh
+++ configs/ubuntu/alias.zsh
@@ -3,12 +3,15 @@ alias -g clip="| xsel -ib"
 alias compresspdf="ps2pdf -dPDFSETTINGS=/ebook"
 alias discord="flatpak run com.discordapp.Discord"
 alias grf="git update-ref -d HEAD"
+alias iphone="cd /run/user/1000/gvfs/afc:host=00008030-001658213AB8402E,port=3/"
+alias j=just
 alias fly=flyctl
 alias fmtsh="shfmt -i 2 -w"
-alias j=just
 alias plm=pulumi
 alias pn=pnpm
 alias reloadzsh="source ~/.zshrc"
 alias rgf="rg --files 2> /dev/null | rg"
+alias thunderbird="flatpak run org.mozilla.Thunderbird"
+alias tui=taskwarrior-tui
 alias update="aguu && sudo $APT dist-upgrade && agar && aga"
 alias zshconfig="open ~/.zshrc"
