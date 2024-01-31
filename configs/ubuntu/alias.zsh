2aabb84ad10ba6e25ae4ba716a8d05898db23ca0
sripwoud <me@sripwoud.xyz>
2024-01-31 13:49:16 +0100

    update ubuntu alias.zsh

--- configs/ubuntu/alias.zsh
+++ configs/ubuntu/alias.zsh
@@ -3,15 +3,16 @@ alias -g clip="| xsel -ib"
 alias compresspdf="ps2pdf -dPDFSETTINGS=/ebook"
 alias discord="flatpak run com.discordapp.Discord"
 alias grf="git update-ref -d HEAD"
-alias iphone="cd /run/user/1000/gvfs/afc:host=00008030-001658213AB8402E,port=3/"
-alias j=just
 alias fly=flyctl
 alias fmtsh="shfmt -i 2 -w"
+alias iphone="cd /run/user/1000/gvfs/afc:host=00008030-001658213AB8402E,port=3/"
+alias j=just
 alias plm=pulumi
+alias poe="poetry run poe"
 alias pn=pnpm
 alias reloadzsh="source ~/.zshrc"
 alias rgf="rg --files 2> /dev/null | rg"
-alias thunderbird="flatpak run org.mozilla.Thunderbird"
-alias tui=taskwarrior-tui
 alias update="aguu && sudo $APT dist-upgrade && agar && aga"
+alias tui=taskwarrior-tui
+alias thunderbird="flatpak run org.mozilla.Thunderbird"
 alias zshconfig="open ~/.zshrc"
