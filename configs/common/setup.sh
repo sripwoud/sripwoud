7e61289f4a53b0bc9d3676d35f7a805623368829
sripwoud <me@sripwoud.xyz>
2024-02-17 10:08:11 +0100

    Update `common` scripts

--- configs/common/setup.sh
+++ configs/common/setup.sh
@@ -2,10 +2,99 @@
 
 set -e
 
-main() {
-  for i in {1..2}; do
-    sudo curl -fsS https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs/common/setup"$i"2.sh | sh
+install_asdf() {
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  touch ~/.tool-versions
+
+  tmp_file=$(mktemp)
+  curl -fsS https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs/common/asdf-plugins >"$tmp_file"
+
+  while read -r line; do
+    read -ra args <<< "$line"
+    plugin=${args[0]}
+    url=${args[1]}
+    branch=${args[2]}
+
+    [[ -n $url ]] && asdf plugin add "$plugin" "$url" | asdf plugin add "$plugin"
+    [[ -n $branch ]] && asdf plugin update "$plugin" "$branch"
+    asdf install "$plugin" latest
+    asdf global "$plugin" latest
+  done <"$tmp_file"
+
+  rm "$tmp_file"
+}
+
+install_circom() {
+  temp_dir=$(mktemp -d)
+  git clone https://github.com/iden3/circom.git "$temp_dir"
+  cd "$temp_dir"
+  cargo build --release
+  cargo install --path circom
+  cd ~
+  rm -rf "$temp_dir"
+}
+
+install_foundry() {
+  curl -L https://foundry.paradigm.xyz | bash
+  source "$HOME"/.zshrc
+  foundryup
+}
+
+get_common_config_files() {
+  for file in .default-npm-packages .npmrc; do
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      curl -o "$HOME/$file" -fsS "https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs/macos/$file"
+    else
+      curl -o "$HOME/$file" -fsS "https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs/manjaro/$file"
+    fi
   done
+
+  mkdir -p "${$HOME/.oh-my-zsh/custom}"/plugins/sha256
+  curl -o "${$HOME/.oh-my-zsh/custom}"/plugins/sha256/sha256.plugin.zsh -fsS "https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs/common/sha256.zsh"
+}
+
+config_ssh() {
+  gh auth login
+  eval "$(ssh-agent -s)"
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+    echo "Host github.com
+     AddKeysToAgent yes
+     UseKeychain yes
+     IdentityFile ~/.ssh/id_ed25519" >>~/.ssh/config
+  else
+    ssh-add ~/.ssh/id_ed25519
+  fi
+}
+
+config_gpg() {
+  gh auth refresh -s write:gpg_key
+
+  gpg --full-generate-key --allow-freeform-uid --expert
+  gpg --list-secret-keys --keyid-format LONG
+
+  read -r -p "Enter the gpg key ID: " key_id
+  git config --global user.signingkey "$key_id"
+
+  read -r -p "Enter title for gpg key on GitHub: " title
+  gpg --armor --export "$key_id" | gh gpg-key add -t "$title"
+  curl https://github.com/web-flow.gpg | gpg --import
+
+  if [[ "$OSTYPE" == "darwin"* ]];then
+    echo "pinentry-program $(which pinentry-mac)" >> ~/.gnupg/gpg-agent.conf
+    gpgconf --kill gpg-agent
+  fi
+}
+
+main() {
+  get_common_config_files
+  install_asdf # asdf is an oh my zsh plugin, and rust is included in asdf plugins
+  install_foundry
+#  install_circom
+  mkdir ~/{dev,filen,.{pyenvs,vpn}}
+  config_ssh
+  config_gpg
 }
 
 main
--- configs/setup.sh
+++ configs/setup.sh
@@ -5,9 +5,9 @@ set -e
 main() {
   local url="https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs"
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -fsS "$url/mac/setup.sh" | sh
+    curl -fsS "$url/macos/setup.sh" | sh
   else
-    curl -fsS "$url/ubuntu/setup.sh" | sh
+    curl -fsS "$url/manjaro/setup.sh" | sh
   fi
 }
 
