1261e42c58a6b3a427b2b9b80fe6fdeabfb67498
sripwoud <me@sripwoud.xyz>
2024-02-17 10:02:25 +0100

    Rename ubuntu folder to manjaro

--- configs/manjaro/setup13.sh
+++ configs/manjaro/setup13.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-add_ppa() {
-  if ! grep -h "deb.*$1" /etc/apt/sources.list.d/* > /dev/null 2>&1;
-   then
-    echo "Adding ppa:$1"
-    sudo add-apt-repository -y "ppa:$1"
-    return 0
-  else
-    echo "ppa:$1 already exists"
-    return 1
-  fi
-}
-
-install_apt_pkgs() {
-  add_ppa universe # required for fira-code font
-  add_ppa ppa:appimagelauncher-team/stable
-  sudo apt update
-  curl -fsS "$url"/ubuntu/apt | sudo xargs apt install -y
-}
-
 get_ubuntu_config_files() {
   for file in .gitconfig .gitignore .zshenv .zshrc; do
     curl -o "$HOME/$file" -fsS "$url/ubuntu/$file"
@@ -31,7 +12,6 @@ get_ubuntu_config_files() {
 
 main() {
   local url=https://raw.githubusercontent.com/sripwoud/sripwoud/main/configs
-  install_apt_pkgs
 
   chsh -s "$(which zsh)"
 
